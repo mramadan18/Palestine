4813647b9be466fef5f1054e98bbfcdfcb55b227
anwar181199 <72145545+anwar181199@users.noreply.github.com>
2021-06-16 14:35:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,3 @@ for (let i = 0; i < 400; i++) {
   box.innerHTML += "<div class='blocks'></div>";
   blocks[i].style.animationDelay = `${i * 0.05}s`;
 };
-
-let audio = document.createElement("audio");
-
-audio.src = "AUDIO.m4a";
-
-audio.play();
