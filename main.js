fb3150c1004fae504540019427896f78f7444851
anwar181199 <72145545+anwar181199@users.noreply.github.com>
2021-06-16 14:17:23 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-let box = document.getElementsByClassName("box")[0],
+let box = document.querySelector(".box"),
     blocks = document.getElementsByClassName("blocks");
+
+console.log(box);
+console.log(blocks);
 
 for (let i = 0; i < 400; i++) {
   box.innerHTML += "<div class='blocks'></div>";
   blocks[i].style.animationDelay = `${i * 0.05}s`;
 }
+
+
+let audio = document.createElement("audio");
+
+audio.src = "AUDIO.m4a"
+
+audio.play();
